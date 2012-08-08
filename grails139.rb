d80020b899a745ffb5893ed39394f5cea39197af
Mika Marttila <mika@enemy.fi>
2012-08-08 10:36:22 +0300

    Added Grails 1.3.9

---
+++ grails139.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Grails139 < Formula
+  homepage 'http://grails.org'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-1.3.9.zip'
+  md5 '628eaab10d033c02e6c2d97bcd7d2464'
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install %w[LICENSE README]
+    libexec.install Dir['*']
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |f|
+      next unless File.extname(f).empty?
+      ln_s f, bin+File.basename(f)
+    end
+  end
+end
